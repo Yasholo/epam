9b8d58eecdd39f7e7827b419686213b48490df7b
Yash <yashk5635@gmail.com>
2023-02-07 17:21:02 +0530

    CA-1 question2

--- ques2.sh
+++ ques2.sh
@@ -1,1 +1,7 @@
+#Question2. You need to find a file named myfile.txt somewhere in your Linux file system. Which is thecorrect command to do this? 
+
+#Code starts from next line here >>>>>>
+
+#!usr/bin/bash
+
 sudo find / -name "myfile.txt"
