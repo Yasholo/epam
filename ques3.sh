cad09f4bd3df48e1234579103412dac7c3d0b5de
Yash <yashk5635@gmail.com>
2023-02-07 17:21:20 +0530

    CA-1 question3

--- ques3.sh
+++ ques3.sh
@@ -1,3 +1,14 @@
+#Question3. Write a scipt to Lock down the file or directory permission for folder1 files as per following requirement  [8]
+#mkdir folder1
+#touch f_{1..3}
+#.file.sh folder1/
+#Remove all permissions for groups and others.
+#Provide directory name through command-line.
+#After running script all files in the given directory, Only should have all the permissions.
+#But remember dont add any permission to user only change to others and groups.
+
+#Code starts from next line here >>>>>>
+
 #!usr/bin/bash
 
 mkdir folder1
