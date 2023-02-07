a12b25bcb7f2c8c139ed50984663d08f0a399b16
Yash <yashk5635@gmail.com>
2023-02-07 17:20:39 +0530

    CA-1 question1

--- ques1.sh
+++ ques1.sh
@@ -1,3 +1,8 @@
+#Question1. write a shell script function that take name of a file or directory as unlimited argument and reports if it is a regular file, a directory, or another type of file.Also perform an ls command against the file or directory with the long listing option. Write for all the mention details . also create function function “file_count” and call it in your script. If you use variable in your function, remember to make it a local variable. [5]
+
+
+#Code starts from next line here >>>>>>
+
 #!usr/bin/bash
 
 fun1(){
